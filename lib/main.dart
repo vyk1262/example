305453ef070c92b7b7f3f9baaf864c593f9af99f
vyk1262 <vyk1262@gmail.com>
2023-06-18 09:27:52 +0530

    Able to get only streaming data per issue

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 import 'teachable.dart';
-// import 'package:teachable/teachable.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Permission.camera.request();
   await Permission.microphone.request();
   runApp(MyApp());
 }
@@ -38,7 +36,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String score = '';
-  // List<dynamic> resp = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,32 +44,32 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             Container(
                 child: Column(children: <Widget>[
-              Expanded(
-                child: Container(
-                  child: Teachable(
-                    path: "assets/index.html",
-                    results: (res) {
-                      var resp = jsonDecode(res);
-                      setState(() {
-                        score = (resp['Background Noise'] * 100.0).toString();
-                      });
-                    },
-                  ),
-                ),
-              ),
               // Expanded(
               //   child: Container(
               //     child: Teachable(
-              //       path: "assets/original.html",
+              //       path: "assets/index.html",
               //       results: (res) {
               //         var resp = jsonDecode(res);
               //         setState(() {
-              //           score = (resp[1] * 100.0).toString();
+              //           score = (resp['Background Noise'] * 100.0).toString();
               //         });
               //       },
               //     ),
               //   ),
               // ),
+              Expanded(
+                child: Container(
+                  child: Teachable(
+                    path: "assets/original.html",
+                    results: (res) {
+                      var resp = jsonDecode(res);
+                      setState(() {
+                        score = (resp[0] * 100.0).toString();
+                      });
+                    },
+                  ),
+                ),
+              ),
             ])),
             Align(
               alignment: Alignment.bottomCenter,
